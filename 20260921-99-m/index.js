34b881003e05a83160b0d7c6fb4cf83abb4abec8
Alan Lee <alan93072@gmail.com>
2026-09-21 16:36:39 +0800

    feat: build 9x9 multiply table

---
+++ 20260921-99-m/index.js
@@ -0,0 +1,9 @@
+const MULTIPLICAND = 9,
+  MULTIPLIER = 9;
+
+for (let m7cand = 1; m7cand <= MULTIPLICAND; m7cand++) {
+  for (let m5plier = 1; m5plier <= MULTIPLIER; m5plier++) {
+    const product = m7cand * m5plier;
+    console.log(`${m7cand} x ${m5plier} = ${product}`);
+  }
+}
